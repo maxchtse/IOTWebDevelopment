8400c01be92e9d27970308194ae09632aa73eb1f
Max Tse <98444048+maxtch@users.noreply.github.com>
2023-09-28 13:44:33 +0800

    Delete AQMS directory

--- AQMS/AQMS/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from base import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    path('download-json/', views.download_json, name='download-json'),
-]
--- AQMS/base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import SensorReading
-
-admin.site.register(SensorReading)
