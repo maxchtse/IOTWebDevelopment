cfa1d8188dd23ec0a01404223404abb30e08b8d8
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-10-08 03:03:25 +0800

    add BME280 sensor data and timestamp

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,6 +1,8 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "Adafruit_CCS811.h"
+#include <time.h>
+char timestamp[64] = ""; 
 
 // =============================
 // ==== Configurable Constants ====
@@ -8,21 +10,21 @@
 const char* ssid = "";
 const char* password = "";
 const char* firebaseUrl = "https://cits5506-default-rtdb.firebaseio.com/sensorData.json";
-#define BUZZZER_PIN 32
+#define BUZZZER_PIN 23
 #define COV_RATIO 0.2 // ug/mmm / mv for dust sensor
 #define NO_DUST_VOLTAGE 10 // Reduced from 400 for dust sensor testing 
 #define SYS_VOLTAGE 3300 // unit: mV for dust sensor
-const int iled = 23; //drive the led of dust sensor
+const int iled = 32; //drive the led of dust sensor
 const int vout = 33; //analog input for dust sensor
 struct SensorThresholds {
-    int densityThreshold;
-    int eCO2Threshold;
-    int TVOCThreshold;
+  int densityThreshold;
+  int eCO2Threshold;
+  int TVOCThreshold;
 };
 SensorThresholds thresholds = {
-    .densityThreshold = 50,
-    .eCO2Threshold = 800,
-    .TVOCThreshold = 300,
+  .densityThreshold = 50,
+  .eCO2Threshold = 800,
+  .TVOCThreshold = 300,
 };
 
 // ==========================
@@ -107,80 +109,144 @@ Adafruit_CCS811 ccs;
 // GPIO 21 for SDA (Default) 
 // GPIO 22 for SCL (Default)
 
+// ===================================
+// === BME280 Sensor Initialization ===
+// ===================================
+#include <Adafruit_BME280.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+
+#define SEALEVELPRESSURE_HPA (1018.8) // Barometric Pressure in Perth, AU
+
+Adafruit_BME280 bme;
+
 // ====================
 // ==== Setup Logic ====
 // ====================
 void setup() {
-    Serial.begin(9600);
-    // Dust Sensor
-    pinMode(iled, OUTPUT);
-    digitalWrite(iled, LOW);
-    // CO2 Sensor
-    if (!ccs.begin()) {
-        Serial.println("Failed to start CCS811!");
-        while (1);
-    }
-    // Calibrate CO2 Sensor: temperature offset
-    while(!ccs.available());
-    float temp = ccs.calculateTemperature();
-    ccs.setTempOffset(temp - 25.0);
-    // WiFi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
-    Serial.println("Connected to WiFi");
+  Serial.begin(9600);
+
+  // Dust Sensor
+  pinMode(iled, OUTPUT);
+  digitalWrite(iled, LOW);
+
+  // CO2 Sensor
+  if (!ccs.begin()) {
+      Serial.println("Failed to start CCS811!");
+      while (1);
+  }
+
+  // Calibrate CO2 Sensor: temperature offset
+  while(!ccs.available());
+  float temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
+  // WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // config time
+  configTime(8 * 3600, 8 * 3600, "pool.ntp.org", "time.nist.gov"); // UTC+8
+  
+  // BME280 Sensor
+  bool status;
+  status = bme.begin(0x76);  
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  }
 }
 
 // =====================
 // ==== Main Looping ====
 // =====================
 void loop() {
-    // Dust Sensor
-    digitalWrite(iled, HIGH); 
-    delayMicroseconds(280); 
-    adcvalue = analogRead(vout); 
-    digitalWrite(iled, LOW); 
-    adcvalue = Filter(adcvalue); 
-    voltage = (SYS_VOLTAGE / 4095.0) * adcvalue * 11; 
-    if (voltage >= NO_DUST_VOLTAGE) { 
-        voltage -= NO_DUST_VOLTAGE; 
-        density = voltage * COV_RATIO; 
-    } else { 
-        density = 0; 
-    }
-    Serial.printf("The current dust concentration is: %f ug/m3\n", density);
-    if (density > thresholds.densityThreshold) playMelody();
+  float Temp = bme.readTemperature();
+  float hPa = bme.readPressure() / 100.0F;
+  float Altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  float Humidity = bme.readHumidity();
+  Serial.print("Temperature: ");
+  Serial.print(Temp);
+  Serial.println(" °C");
 
-    // CO2 Sensor
-    int eCO2 = 0, TVOC = 0;
-    if (ccs.available()) {
-        float temp = ccs.calculateTemperature();
-        if (!ccs.readData()) {
-            eCO2 = ccs.geteCO2();
-            TVOC = ccs.getTVOC();
-            Serial.printf("CO2: %d ppm\n", eCO2);
-            Serial.printf("TVOC: %d\n", TVOC);
-            if (eCO2 > thresholds.eCO2Threshold) playMelody();
-            if (TVOC > thresholds.TVOCThreshold) playMelody();
-        } else {
-            Serial.println("Error reading CCS811 data!");
-        }
-    }
+  Serial.print("Pressure: ");
+  Serial.print(hPa);
+  Serial.println(" hPa");
 
-    // Send data to Firebase
-    String jsonData = "{\"eCO2\":" + String(eCO2) + ",\"TVOC\":" + String(TVOC) + ",\"DustDensity\":" + String(density) + "}";
-    HTTPClient http;
-    http.begin(firebaseUrl);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(jsonData);
-    if (httpResponseCode == 200) {
-        Serial.println("Data sent to Firebase!");
-    } else {
-        Serial.println("Error sending data: ");
-        Serial.println(httpResponseCode);
-    }
-    http.end();
-    delay(1000);
+  Serial.print("Altitude: ");
+  Serial.print(Altitude);
+  Serial.println(" meters");
+
+  Serial.print("Humidity: ");
+  Serial.print(Humidity);
+  Serial.println(" %");
+
+  // Dust Sensor
+  digitalWrite(iled, HIGH); 
+  delayMicroseconds(280); 
+  adcvalue = analogRead(vout); 
+  digitalWrite(iled, LOW); 
+  adcvalue = Filter(adcvalue); 
+  voltage = (SYS_VOLTAGE / 4095.0) * adcvalue * 11; 
+  if (voltage >= NO_DUST_VOLTAGE) { 
+      voltage -= NO_DUST_VOLTAGE; 
+      density = voltage * COV_RATIO; 
+  } else { 
+      density = 0; 
+  }
+  Serial.printf("The current dust concentration is: %f ug/m3\n", density);
+  if (density > thresholds.densityThreshold) playMelody();
+
+  // CO2 Sensor
+  int eCO2 = 0, TVOC = 0;
+  if (ccs.available()) {
+      float temp = ccs.calculateTemperature();
+      if (!ccs.readData()) {
+          eCO2 = ccs.geteCO2();
+          TVOC = ccs.getTVOC();
+          Serial.printf("CO2: %d ppm\n", eCO2);
+          Serial.printf("TVOC: %d\n", TVOC);
+          if (eCO2 > thresholds.eCO2Threshold) playMelody();
+          if (TVOC > thresholds.TVOCThreshold) playMelody();
+      } else {
+          Serial.println("Error reading CCS811 data!");
+      }
+  }
+
+  // timestamp
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+  }
+  else {
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &timeinfo);
+    Serial.println(timestamp);
+  }
+
+  // Send data to Firebase
+  String jsonData = "{"
+    "\"timestamp\":\"" + String(timestamp) + "\","
+    "\"Temperature\":" + String(Temp) + ","
+    "\"Pressure\":" + String(hPa) + ","
+    "\"Altitude\":" + String(Altitude) + ","
+    "\"Humidity\":" + String(Humidity) + ","
+    "\"eCO2\":" + String(eCO2) + ","
+    "\"TVOC\":" + String(TVOC) + ","
+    "\"DustDensity\":" + String(density) +
+  "}";
+
+  HTTPClient http;
+  http.begin(firebaseUrl);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(jsonData);
+  if (httpResponseCode == 200) {
+      Serial.println("Data sent to Firebase!");
+  } else {
+      Serial.println("Error sending data: ");
+      Serial.println(httpResponseCode);
+  }
+  http.end();
+  delay(1000);
 }
