3820d252dfb215b4f60262f85feb61960b3bf8fe
Hamza Usmani <23685219@student.uwa.edu.au>
2023-10-11 00:53:01 +0800

    Testing with Firebase data fixed app.py bugs and fixed timestamp format issue

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, make_response, json
 from connection import get_sensor_data
 from datetime import datetime
+from dateutil import parser
+
 
 
 app = Flask(__name__)
@@ -22,7 +24,7 @@ def data():
     # Fetch the sensor data
     data_str = get_sensor_data()  ##IF YOU WANT TO GET THE DATA FROM FIREBASE
     import json
-    data_str = ''
+    
 # Open and read the JSON file
 
     #the code below is for testing
@@ -31,9 +33,10 @@ def data():
     #     data_str = json.load(file)
     
     # Extract the entry with the latest timestamp
+    print(data_str)
     latest_entry = max(
         data_str.values(),
-        key=lambda x: datetime.strptime(x["timestamp"], "%Y-%m-%dT%H:%M:%S%z"),
+        key=lambda x: parser.parse(x["timestamp"]),
     )
     # Extract TVOC, eCO2, humidity and temperature for Line Chart
     latest_TVOC = latest_entry["TVOC"]
@@ -109,7 +112,7 @@ def data():
 
     # Prepare data in the required format: [Timestamp, TVOC, eCO2]
     # Convert the timestamp string to a datetime object
-    timestamp_dt = datetime.strptime(latest_entry["timestamp"], "%Y-%m-%dT%H:%M:%S%z")
+    timestamp_dt = parser.parse(latest_entry["timestamp"])
     
     # Convert the datetime object to a timestamp (in milliseconds)
     timestamp_ms = timestamp_dt.timestamp() * 1000
